4d879575a0b5fa404ee87920a8190e79801d5fd7
Itay Saig <117663458+Itay-Saig@users.noreply.github.com>
2023-06-19 22:20:47 +0300

    Update USA_borrowers_visualizations.py

--- USA_borrowers_visualizations.py
+++ USA_borrowers_visualizations.py
@@ -351,7 +351,7 @@ with st.container():
                 "legend": {
                     "data": loan_status_values
                 },
-                "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+                "grid": {"left": "3%", "right": "4%", "bottom": "30%", "containLabel": True},
                 "xAxis": {
                     "type": "category",
                     "data": income_labels,
