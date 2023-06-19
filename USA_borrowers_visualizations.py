54609371a3c764424555e302fcadc35878f4e973
Itay Saig <117663458+Itay-Saig@users.noreply.github.com>
2023-06-19 21:42:11 +0300

    Update USA_borrowers_visualizations.py

--- USA_borrowers_visualizations.py
+++ USA_borrowers_visualizations.py
@@ -751,9 +751,9 @@ option = {
         "name": "The Percentage of Borrowers",
         "nameLocation": "middle",
         "nameGap": 40,
-        "interval": 5  # Set the interval to 5 to make the spacing smaller
     },
-    "grid": {"top": "10%", "bottom": "5%",
+    "grid": {"top": "10%",
+             "bottom": "20%",
     },
     "series": [
         {"type": "line", "smooth": True, "seriesLayoutBy": "row", "emphasis": {"focus": "series"}},
