6db48f0307c1fa022d0171de816cb2317419c2d8
Itay Saig <117663458+Itay-Saig@users.noreply.github.com>
2023-06-19 22:19:01 +0300

    Update USA_borrowers_visualizations.py

--- USA_borrowers_visualizations.py
+++ USA_borrowers_visualizations.py
@@ -343,7 +343,7 @@ with st.container():
                 "title": {
                     "text": "Loan Repayment by Borrower's Annual Income: Analysis of Loan Closings and Income Trends",
                     "left": "center",
-                    "top": "-10%",
+                    "top": "-5%",
                     "padding": 20,
                     "textStyle": {"fontWeight": "bold"}
                 },
