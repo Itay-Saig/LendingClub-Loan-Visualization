1f2b15e17287236072c303779e41166fcd2a4de3
Itay Saig <117663458+Itay-Saig@users.noreply.github.com>
2023-06-19 22:07:20 +0300

    Update USA_borrowers_visualizations.py

--- USA_borrowers_visualizations.py
+++ USA_borrowers_visualizations.py
@@ -202,7 +202,7 @@ def render_usa():
                 "roam": True,
                 "map": "USA",
                 "top": "20%",
-                "right": "30%",
+                "right": "15%",
                 "emphasis": {"label": {"show": True}},
                 "textFixed": {"Alaska": [20, -20]},
                 "data": [
