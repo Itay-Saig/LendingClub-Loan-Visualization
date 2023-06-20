823452c34ff6b05159675a5a958d493abf7579d4
Itay Saig <117663458+Itay-Saig@users.noreply.github.com>
2023-06-21 02:27:53 +0300

    Update USA_borrowers_visualizations.py

--- USA_borrowers_visualizations.py
+++ USA_borrowers_visualizations.py
@@ -36,7 +36,7 @@ with st.container():
     st.write("\n")
 
 st.header("Overview")
-st.subheader("""The subject of our simulation is to examine the change in characteristics of borrowers in the United States over the years in various aspects.""")
+st.subheader("<h3 style='font-size: 16px;'>The subject of our simulation is to examine the change in characteristics of borrowers in the United States over the years in various aspects.</h3>", unsafe_allow_html=True)
 st.subheader("""Our visualization theme focuses on the ‘Lending Club Loan’ dataset from Kaggle which contains data from LendingClub company. LendingClub is a financial services company headquartered in San Francisco, California. The data available on Kaggle is a comprehensive dataset of loan information from LendingClub from 2007 to 2018.""")
 # st.subheader("""Music, often revered as a universal language, has been used for centuries to convey emotions and thoughts. Music is valued, among other things, for its therapeutic potential, especially in the treatment of mental health problems in situations such as depression, anxiety, post-traumatic stress disorder (PTSD) and more. However, the mechanisms underlying music's apparent positive effects on mental health remain elusive and unclear to this day.""")
 st.subheader("""The main question we would like to investigate is: In what and how the characteristics of borrowers in the US change over the years?""")
