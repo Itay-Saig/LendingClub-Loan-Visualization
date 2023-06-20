dad1d4ce3e5cd38ab9932a36ad52feb12a32256e
Itay Saig <117663458+Itay-Saig@users.noreply.github.com>
2023-06-21 01:13:58 +0300

    Update USA_borrowers_visualizations.py

--- USA_borrowers_visualizations.py
+++ USA_borrowers_visualizations.py
@@ -214,7 +214,7 @@ def render_usa():
                 "emphasis": {"label": {"show": True}},
                 "textFixed": {"Alaska": [20, -20]},
                 "data": [
-                    {"name": "Alabama", "value": percentage_borrowers_per_state["Alabama"]},
+                    {"name": "Alabama", "value": str(percentage_borrowers_per_state["Alabama"])},
                     {"name": "Alaska", "value": percentage_borrowers_per_state["Alaska"]},
                     {"name": "Arizona", "value": percentage_borrowers_per_state["Arizona"]},
                     {"name": "Arkansas", "value": percentage_borrowers_per_state["Arkansas"]},
